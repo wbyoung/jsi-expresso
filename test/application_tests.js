60874d46efff59c794520165ff94f09115ea098e
Whitney Young <wbyoung@fadingred.com>
2014-10-07 16:23:02 -0700

    Actually sending requests & responses.

--- test/application_tests.js
+++ test/application_tests.js
@@ -4,6 +4,7 @@ var chai = require('chai');
 chai.use(require('sinon-chai'));
 
 var expect = chai.expect;
+var http = require('http');
 var sinon = require('sinon');
 var Application = require('../lib/application');
 
@@ -33,6 +34,32 @@ describe('Application', function() {
     var server = app.listen(38292, function() {
       server.close(function() {
         done();
+      });
+    });
+  });
+
+  it('dispatches requests properly', function(done) {
+    var spy = sinon.spy(function(req, res) {
+      res.write('Hello world');
+      res.end();
+    });
+    var app = new Application();
+    app.get('/index.html', spy);
+    var server = app.listen(38292, function() {
+
+      http.get('http://localhost:38292/index.html', function(res) {
+        expect(spy).to.have.been.calledOnce;
+
+        var responseString = '';
+        res.on('data', function(moreData) {
+          responseString += moreData.toString();
+        });
+        res.on('end', function() {
+          expect(responseString).to.eql('Hello world');
+          server.close(function() {
+            done();
+          });
+        });
       });
     });
   });
